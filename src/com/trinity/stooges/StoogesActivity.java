474cfa95c69ec3e011d33eafc5b804969691f28f
Sandeep Raghuraman <sandy.8925@gmail.com>
2012-03-24 00:50:40 -0400

    Added xml layout for individual list element. Also added code to main activity to display list of playlists from database

--- src/com/trinity/stooges/StoogesActivity.java
+++ src/com/trinity/stooges/StoogesActivity.java
@@ -1,13 +1,28 @@
 package com.trinity.stooges;
 
 import android.app.ListActivity;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.widget.SimpleCursorAdapter;
 
 public class StoogesActivity extends ListActivity {
+	private MusicDbAdapter databaseHelper;
+	private Cursor playlist_list_cursor;
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        databaseHelper = new MusicDbAdapter(this);
+        databaseHelper.open();
+        
+       playlist_list_cursor = databaseHelper.fetchAllPlaylists();
+       startManagingCursor(playlist_list_cursor);
+       
+       String from[] = new String[] {MusicDbAdapter.playlist_table_namecol};
+       int[] to = new int[] {0};
+   	   SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.listrow, playlist_list_cursor, from, to);
+   	   setListAdapter(notes);
     }
 }
