93aabc6c26d22e225ec8f1078f783f4d59008f61
Sandeep Raghuraman <sandy.8925@gmail.com>
2012-03-24 03:36:40 -0400

    Replaced 0 with the correct resource id in StoogesActivity.java. Fixes a bug which makes the app crash

--- src/com/trinity/stooges/StoogesActivity.java
+++ src/com/trinity/stooges/StoogesActivity.java
@@ -12,17 +12,18 @@ public class StoogesActivity extends ListActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        databaseHelper = new MusicDbAdapter(this);
-        databaseHelper.open();
+       super.onCreate(savedInstanceState);
+       setContentView(R.layout.main);
+       databaseHelper = new MusicDbAdapter(this);
+       databaseHelper.open();
         
        playlist_list_cursor = databaseHelper.fetchAllPlaylists();
        startManagingCursor(playlist_list_cursor);
        
        String from[] = new String[] {MusicDbAdapter.playlist_table_namecol};
-       int[] to = new int[] {0};
+       int[] to = new int[] {R.id.list_row};
    	   SimpleCursorAdapter notes = new SimpleCursorAdapter(this, R.layout.listrow, playlist_list_cursor, from, to);
    	   setListAdapter(notes);
     }
+    
 }
